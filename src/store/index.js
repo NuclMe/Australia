a352275576d1635963e7a73eefae7043958de0f1
NuclMe <you@example.com>
2021-06-26 14:55:21 +0300

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+const countReducer = function(state=0,action){
+    switch(action.type){
+        case 'plus':
+            return state + 1;
+        case 'minus':
+            return state -1;
+        default:
+            return state;
+    }
+};
